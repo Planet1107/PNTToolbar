1998582c38d1ddb96156092a1af49ca7ef97b36d
Josip Cavar <josipcavar04@gmail.com>
2014-05-12 12:50:23 +0200

    added podspec file

--- PNTToolbarDemo/PNTToolbarDemo/ViewController/PNTHomeViewController.m
+++ PNTToolbarDemo/PNTToolbarDemo/ViewController/PNTHomeViewController.m
@@ -2,7 +2,7 @@
 //  PNTHomeViewController.m
 //  PNTToolbarDemo
 //
-//  Created by Josip Ćavar on 21/11/13.
+//  Created by Planet 1107 on 21/11/13.
 //  Copyright (c) 2013 Planet 1107. All rights reserved.
 //
 
