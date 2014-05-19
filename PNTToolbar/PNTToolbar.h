c95733058ce4ef4730ee0750b5968388786f91f6
Josip Cavar <josipcavar04@gmail.com>
2014-05-19 10:26:05 +0200

    code style

--- PNTToolbar/PNTToolbar.h
+++ PNTToolbar/PNTToolbar.h
@@ -1,11 +1,8 @@
 //
-//  PLToolbar.h v1.3
+//  PLToolbar.h
 //
-//  Created by Planet 1107 on 11/4/13.
+//  Created by Planet 1107
 //
-
-// v 1.3 11/4/13 - Added support for ipad and landscape orientation
-// TO DO - Need to track orientation changes and scroll depending on it. Now it works for both orientations if there is no change
 
 #define KEYBOARD_ANIMATION_DURATION 0.4
 
@@ -13,7 +10,7 @@
 
 @interface PNTToolbar : UIToolbar
 
-@property (strong, nonatomic) UIScrollView* mainScrollView;
+@property (strong, nonatomic) UIScrollView *mainScrollView;
 @property (strong, nonatomic) NSArray *textFields;
 @property (strong, nonatomic) NSArray *delegates;
 @property (assign, nonatomic) BOOL shouldHideNavigationButtons;
